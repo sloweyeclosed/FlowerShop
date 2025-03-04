2c99bb8173a74d640c43a38894447ef79564a2e2
andrey <sloweyeclose@gmail.com>
2025-03-04 19:16:25 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openmodal() {
+  let modal = document.getElementById("modal");
+  modal.style.display = "flex";
+}
+function closemodal() {
+  let modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
