1b904b03eb8efa07b7790402c33259b8e921f95e
andrey <sloweyeclose@gmail.com>
2025-03-04 20:34:32 +0200

    add sec flower and fix footer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 function openmodal() {
   let modal = document.getElementById("modal");
+  let body = document.getElementById("html");
   modal.style.display = "flex";
+  body.style.overflowY = "hidden";
 }
 function closemodal() {
   let modal = document.getElementById("modal");
+  let body = document.getElementById("html");
   modal.style.display = "none";
+  body.style.overflowY = "scroll";
 }
